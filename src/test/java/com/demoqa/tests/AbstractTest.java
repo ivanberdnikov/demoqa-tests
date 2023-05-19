63a9dc96259d0ff0431937db1ba64f37df7213c8
ivanberdnikov <mr.berdnikov1998@mail.ru>
2023-05-19 19:34:40 +0300

    commit

--- src/test/java/com/demoqa/tests/AbstractTest.java
+++ src/test/java/com/demoqa/tests/AbstractTest.java
@@ -1,13 +1,27 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class AbstractTest {
 
+
     @BeforeAll
-    static void configuration() {
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadTimeout  = 60000;
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
